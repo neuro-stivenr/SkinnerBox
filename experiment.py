531162ae578d7bc9f70d326ee57cd59ab91c9518
Stiven Roytman <stivenr@Stivens-MacBook-Pro.local>
2022-04-03 02:26:46 -0400

    Dynamic probabilistic ratio trial is implemented.

--- experiment.py
+++ experiment.py
@@ -1,9 +1,21 @@
 import pygame
 import pandas as pd
+import numpy as np
+from numpy.random import shuffle
 from typing import List, Tuple
+from schedules import ProbRatioBlock
 from graphics import COLOR_black, COLOR_red, COLOR_green, COLOR_white
 from graphics import draw_text, draw_option, draw_timer, draw_score
 from graphics import circle_coords
+
+def calc_score_ratio(df_log:pd.DataFrame):
+    dist = df_log.press.value_counts()
+    dominant_option = dist.argmax()
+    distlist = dist.tolist()
+    dominant_score = distlist.pop(dominant_option)
+    others_score = np.sum(distlist)
+    score_ratio = dominant_score / (others_score+1e-5)
+    return score_ratio
 
 pygame.init()
 
@@ -37,9 +49,11 @@ class Trial:
         # scorekeeping
         self.score = 0
         # for data output
-        self.timestamp = []
-        self.presslog = []
-        self.outcomelog = []
+        self.output = {
+            'time': [],
+            'press': [],
+            'outcome': []
+        }
 
     def press(self, key:int):
         """
@@ -49,9 +63,9 @@ class Trial:
             chosen_option = self.keymap[key]
             chosen_schedule = self.schedules[chosen_option]
             earned_point = chosen_schedule.press()
-            self.timestamp.append(self.t_elapsed)
-            self.presslog.append(chosen_option+1)
-            self.outcomelog.append(earned_point)
+            self.output['time'].append(self.t_elapsed / 1000)
+            self.output['press'].append(chosen_option+1)
+            self.output['outcome'].append(earned_point)
             if earned_point:
                 self.colormap[chosen_option] = COLOR_green
                 self.score_color = COLOR_green
@@ -83,12 +97,8 @@ class Trial:
         self.score_color = COLOR_black
 
     def return_log(self):
-        return pd.DataFrame({
-            'time': self.timestamp,
-            'press': self.presslog,
-            'outcome': self.outcomelog
-        })
-
+        return pd.DataFrame(self.output)
+        
     def return_schedules(self):
         return pd.DataFrame({
             'schedule': [i+1 for i in range(len(self.schedules))],
@@ -101,10 +111,53 @@ class Trial:
         """
         return len(self.schedules)
 
-class Message:
+class DynamicProbTrial(Trial):
+    def __init__(
+        self, 
+        ratiolist:List[float], 
+        t:int, 
+        shift_limit:int=3,
+        warmup:int=50,
+        inequality_threshold:float = 3.0
+    ):
+        schedules = ProbRatioBlock(ratiolist)
+        super().__init__(schedules, t)
+        self.ratiolist = ratiolist
+        self.schedule_history = [[schedule.p for schedule in schedules]]
+        self.shift_history = [0]
+        self.shift_limit = shift_limit
+        self.warmup_size = warmup
+        self.warmup = warmup
+        self.inequality_threshold = inequality_threshold
+    def shuffle_contingencies(self):
+        arr_schedules = np.array([schedule.p for schedule in self.schedules])
+        best_option = arr_schedules.max()
+        best_option_index = arr_schedules.argmax()
+        while self.schedules[best_option_index].p == best_option:
+            self.schedules = ProbRatioBlock(self.ratiolist)
+    def press(self, key:int):
+        super(DynamicProbTrial, self).press(key)
+        # TODO: uncomment and indent next block
+        if self.warmup > 0:
+            self.warmup -= 1
+        if (len(self.shift_history)-1) < self.shift_limit and self.warmup == 0:
+            df_log = pd.DataFrame(self.output)
+            df_log = df_log[df_log.time > self.shift_history[-1]]
+            score_ratio = calc_score_ratio(df_log)
+            print(score_ratio)
+            if score_ratio > self.inequality_threshold:
+                print(f"SHIFT: {self.t_elapsed / 1000}")
+                self.shuffle_contingencies()
+                self.schedule_history.append([schedule.p for schedule in self.schedules])
+                self.shift_history.append(self.t_elapsed / 1000)
+                self.warmup = self.warmup_size
 
+class Message:
+    # TODO: Add support foor multiline messages.
     def __init__(self, text:str):
         self.text = text
+
+# --- DESIGN ---
 
 class Design:
 
@@ -112,12 +165,15 @@ class Design:
         self, 
         design:List[Trial],
         screen_dims:Tuple[int,int] = (800,800),
-        circle_radius:int = 300
+        circle_radius:int = 300,
+        show_timer:bool = False
     ):
         self.design = design
         self.circle_radius = circle_radius
         self.screen_dims = screen_dims
+        self.screen_center = (screen_dims[0]/2, screen_dims[1]/2)
         self.screen = pygame.display.set_mode(screen_dims)
+        self.show_timer = show_timer
 
     def write_output(self, prefix:str='Subject'):
         for i,trial in enumerate(filter(
@@ -140,17 +196,15 @@ class Design:
                     self.screen.fill(COLOR_white)
                     draw_text(
                         self.screen,
-                        self.screen_dims[0] / 2,
-                        self.screen_dims[1] / 2,
+                        *self.screen_center,
                         block.text
                     )
                     pygame.display.flip()
 
             # Handling Trial blocks
-            elif type(block) == Trial:
+            elif issubclass(type(block), Trial):
                 schedule_coords = circle_coords(
-                    center_x = self.screen_dims[0] / 2, 
-                    center_y = self.screen_dims[1] / 2,
+                    *self.screen_center,
                     r = self.circle_radius,
                     n = len(block)
                 )
@@ -175,20 +229,19 @@ class Design:
 
                     draw_score(
                         self.screen,
-                        self.screen_dims[0] / 2, 
-                        self.screen_dims[1] / 2, 
+                        *self.screen_center,
                         block.score, score_color=block.score_color
                     )
-
-                    draw_timer(
-                        self.screen, 
-                        self.screen_dims[0] / 2, 
-                        (self.screen_dims[1] / 2) + 50, 
-                        block.t
-                    )
+                    if self.show_timer:
+                        draw_timer(
+                            self.screen, 
+                            self.screen_center[0], 
+                            (self.screen_dims[1] / 2) + 50, 
+                            block.t
+                        )
 
                     pygame.display.flip()
-                    tdelta = clock.tick()
+                    tdelta = clock.tick(30)
                     done = block.tick(tdelta)
 
         self.write_output('Steve')
--- game.py
+++ game.py
@@ -1,14 +1,13 @@
-from experiment import Design, Trial, Message
+from experiment import Design, Message, DynamicProbTrial
 from schedules import ProbRatioBlock
 
 my_experiment = Design([
-    Message('Press ENTER to continue.'),
-    Trial(ProbRatioBlock([0.1, 0.2, 0.5, 0.3]), t=20),
-    Message('Press ENTER to continue.'),
-    Trial(ProbRatioBlock([0.1, 0.2, 0.5, 0.3, 0.8]), t=20),
-    Message('Press ENTER to continue.'),
-    Trial(ProbRatioBlock([0.9, 0.5, 0.3, 0.8]), t=20),
-    Message('Experiment completed. Press ENTER to exit.')
+    Message('Press ENTER to start.'),
+    DynamicProbTrial(
+        [0.1, 0.15, 0.2, 0.25, 0.3, 0.6],
+        t=120, warmup=75, inequality_threshold=2.0
+    ),
+    Message('Press ENTER to exit.')
 ])
 
 my_experiment.run()
